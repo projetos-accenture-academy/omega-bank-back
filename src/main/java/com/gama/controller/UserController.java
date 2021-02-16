033a10da22d034de243c8227c66b008506205199
Raul Castro <raulc27@gmail.com>
2021-02-16 00:01:20 -0300

    ultimas alterações

--- src/main/java/com/gama/controller/UserController.java
+++ src/main/java/com/gama/controller/UserController.java
@@ -1,17 +1,23 @@
 package com.gama.controller;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gama.model.Usuario;
 import com.gama.repository.UsuarioRepository;
+
+
 
 
 
@@ -24,12 +30,24 @@ public class UserController  {
 	private UsuarioRepository service;	
 	
 	
+	
+	
 	@PostMapping()
 	public void post(@RequestBody Usuario usuario){
-		service.save(usuario);
+		this.service.save(usuario);
 	}
 	
+	@PutMapping()
+	public void put(@RequestBody Usuario usuario){
+		
+		this.service.save(usuario);
+	}
 	
+	@GetMapping(path = "/{usuario}") //@PathVariable
+	public Optional<Usuario> listar(@PathVariable String usuario){
+		System.out.println("Buscando usuario " + usuario);
+		return service.findByLogin(usuario);
+	}
 	
 	
 	
