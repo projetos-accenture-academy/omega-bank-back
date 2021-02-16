89d8a7197f61f77e8d32175014db2ba52cc39674
Raul Castro <raulc27@gmail.com>
2021-02-15 22:37:39 -0300

    atualizado

--- src/main/java/com/gama/controller/UserController.java
+++ src/main/java/com/gama/controller/UserController.java
@@ -1,5 +1,39 @@
 package com.gama.controller;
 
-public class UserController {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gama.model.Usuario;
+import com.gama.repository.UsuarioRepository;
+
+
+
+
+@RestController
+@RequestMapping(path = "/usuario")
+public class UserController  {
+
+	@Autowired
+	private UsuarioRepository service;	
+	
+	
+	@PostMapping()
+	public void post(@RequestBody Usuario usuario){
+		service.save(usuario);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
