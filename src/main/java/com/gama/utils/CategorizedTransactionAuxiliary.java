b0f8d94bf47bf5aeb9afb5ea8aca8257b84d643d
bconfessor <brian_confessor@poli.ufrj.br>
2021-02-18 15:04:29 -0300

    Update CategorizedTransactionAuxiliary.java
    Mudança de nomes de variáveis

--- src/main/java/com/gama/utils/CategorizedTransactionAuxiliary.java
+++ src/main/java/com/gama/utils/CategorizedTransactionAuxiliary.java
@@ -1,28 +1,35 @@
 package com.gama.utils;
 
+import javax.persistence.Entity;
+
 //Used by TransactionRepo and Service classes to get the sum of values from an account, categorized by account plan
+
+
 public class CategorizedTransactionAuxiliary {
 
-	private Double valueSum;
-	private String transactionTypeDescription;
+	private Double value;
+	
+	private String description;
 	
 	public Double getValueSum() {
-		return valueSum;
+		return value;
 	}
 	public void setValueSum(Double valueSum) {
-		this.valueSum = valueSum;
+		this.value = valueSum;
 	}
 	public String getTransactionTypeDescription() {
-		return transactionTypeDescription;
+		return description;
 	}
 	public void setTransactionTypeDescription(String transactionTypeDescription) {
-		this.transactionTypeDescription = transactionTypeDescription;
+		this.description = transactionTypeDescription;
 	}
+	
+	public CategorizedTransactionAuxiliary() {}
 	
 	public CategorizedTransactionAuxiliary(Double valueSum, String transactionTypeDescription) {
 		
-		this.valueSum = valueSum;
-		this.transactionTypeDescription = transactionTypeDescription;
+		this.value = valueSum;
+		this.description = transactionTypeDescription;
 	}
 	
 	
