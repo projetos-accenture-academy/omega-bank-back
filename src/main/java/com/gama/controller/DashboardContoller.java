14b2eadc4f8f3d5c387e4e6a9af0717175bf4560
kellisonjk <kellisonjk@gmail.com>
2021-02-18 14:34:29 -0300

    Dashboard

---
+++ src/main/java/com/gama/controller/DashboardContoller.java
@@ -0,0 +1,79 @@
+package com.gama.controller;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gama.model.Account;
+import com.gama.model.dto.AccountDTO;
+import com.gama.model.dto.DashboardDTO;
+import com.gama.model.dto.TransactionDTO;
+import com.gama.service.AccountService;
+import com.gama.service.TransactionService;
+
+@Controller
+@RestController
+@RequestMapping(path = "/dashboard")
+public class DashboardContoller {
+
+	@Autowired
+	private TransactionService transactionService;
+
+	@Autowired
+	private AccountService accountService;
+	
+
+	@CrossOrigin
+	@GetMapping(produces="application/json")
+	public Object getDashboard(@RequestParam("login") String user, 
+			@RequestParam("dataInicial") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dateInitial, 
+			@RequestParam("dataFinal") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dateEnd) throws Exception
+	{
+
+		try {
+			if(dateInitial.isAfter(dateEnd))
+				return new ResponseEntity<>("A data de fim não pode ser menor que a de início", HttpStatus.ACCEPTED);
+
+			
+			DashboardDTO dash = new DashboardDTO();
+			
+			List<AccountDTO> accounts = new ArrayList<AccountDTO>();
+			
+			for (Account account : accountService.getAccountsByUserId(1)) {
+
+				List<TransactionDTO> trsDto = (List<TransactionDTO>) transactionService.getAllTransactions();
+				
+				AccountDTO accDto = DashboardDTO.transformToAccountDTO(account, trsDto);
+			
+				accounts.add(accDto);
+			}
+			
+			dash.setNumberOfAccounts(accounts.size());		
+			dash.setAccounts(accounts);
+			
+			//return dash;
+			return new ResponseEntity<>(dash, HttpStatus.OK);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+}
--- src/main/java/com/gama/model/dto/DashboardDTO.java
+++ src/main/java/com/gama/model/dto/DashboardDTO.java
@@ -2,9 +2,19 @@ package com.gama.model.dto;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gama.enums.TransactionType;
+import com.gama.model.Account;
+import com.gama.model.Transaction;
+
 public class DashboardDTO {
 	private List<AccountDTO> accounts;
-	private Integer nuberOfAccounts;
+	private Integer numberOfAccounts;
+
+	@Autowired
+	private ModelMapper modelMapper = new ModelMapper();
 	
 	public DashboardDTO() {
 		
@@ -13,7 +23,7 @@ public class DashboardDTO {
 	public DashboardDTO(List<AccountDTO> accounts, Integer nuberOfAccounts) {
 		super();
 		this.accounts = accounts;
-		this.nuberOfAccounts = nuberOfAccounts;
+		this.numberOfAccounts = nuberOfAccounts;
 	}
 
 	public List<AccountDTO> getAccounts() {
@@ -24,15 +34,23 @@ public class DashboardDTO {
 		this.accounts = accounts;
 	}
 
-	public Integer getNuberOfAccounts() {
-		return nuberOfAccounts;
+	public Integer getNumberOfAccounts() {
+		return numberOfAccounts;
 	}
 
-	public void setNuberOfAccounts(Integer nuberOfAccounts) {
-		this.nuberOfAccounts = nuberOfAccounts;
+	public void setNumberOfAccounts(Integer nuberOfAccounts) {
+		this.numberOfAccounts = nuberOfAccounts;
 	}
+
+
 	
-	
+	public static AccountDTO transformToAccountDTO(Account account, List<TransactionDTO> transactionsDTO )
+	{
+		return new AccountDTO(account.getId(), account.getTipo(), account.getNumero(), 
+				account.getSaldo(), account.getDescricao(), account.getUsuario().getId(), transactionsDTO);
+				
+ 
+	}
 	
 	
 }
